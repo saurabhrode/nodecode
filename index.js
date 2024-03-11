47da6c0838112557fb3faf04e9df136b263d720f
saurabhrode <123395563+saurabhrode@users.noreply.github.com>
2024-03-11 15:42:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Define the route for the root path ("/")
 app.get('/', (req, res) => {
-    const sentence = 'Welcome to  CICD world';
+    const sentence = 'Welcome to  AWS CICD world';
 
     // Send the sentence as the response
     res.json({ sentence });
